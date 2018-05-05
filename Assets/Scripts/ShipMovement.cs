41cad1cd4074b61dbc938e45d66243947f6a9757
macky90078 <mackenzie.w115@gmail.com>
2018-05-04 21:03:32 -0400

    Mack pushing shit

---
+++ Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour {
+
+	[SerializeField]
+	private float m_forwardSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		MoveShipForward ();
+	}
+
+    private void MoveShipForward()
+    {
+		transform.Translate (Vector3.forward * ( m_forwardSpeed * Time.deltaTime));
+    }
+}
