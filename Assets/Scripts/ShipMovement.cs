963caaae5d02961d787eabdafafecedad45a3773
macky90078 <mackenzie.w115@gmail.com>
2018-05-04 23:50:04 -0400

    We can build for Android now...yay

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -7,9 +7,17 @@ public class ShipMovement : MonoBehaviour {
 	[SerializeField]
 	private float m_forwardSpeed;
 
+	[SerializeField]
+	private float m_sideSpeed;
+
+	private float m_sideMoveThreashold = 0.2f;
+	private float m_sideMoveX;
+	private float m_iPx;
+
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
 	}
 	
 	// Update is called once per frame
@@ -22,4 +30,16 @@ public class ShipMovement : MonoBehaviour {
     {
 		transform.Translate (Vector3.forward * ( m_forwardSpeed * Time.deltaTime));
     }
+
+	private void ShipGyro()
+	{
+		m_sideMoveX = 0;
+		m_iPx = Input.acceleration.x;
+
+		if (Mathf.Abs (m_iPx) > m_sideMoveThreashold) 
+		{
+			m_sideMoveX = Mathf.Sign (m_iPx) * m_sideMoveX;
+			transform.Translate (m_sideMoveX, 0, 0);
+		}
+	}
 }
