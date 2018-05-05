228c25b4621b28c3671867efdc3a51c34e5f4b24
Robmastro <rob.mastroieni@gmail.com>
2018-05-04 21:45:58 -0400

    Camera Follow

---
+++ Assets/Scripts/CamFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour {
+
+    public GameObject Player;
+    public Transform CamPos;
+    public Vector3 NewCamPos;
+
+    public float CamHeight;
+    public float CamHorizontal;
+    
+    // Use this for initialization
+	void Start () {
+        CamHeight = 1.18f; 
+        CamHorizontal = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        NewCamPos = new Vector3(CamHorizontal, CamHeight, Player.transform.position.z);
+        CamPos.position = NewCamPos;
+	}
+}
