436b5b9eac2351a3a304425339ac739cb3f8385a
RashadMohammed <rish.mohammed@gmail.com>
2018-05-05 21:13:20 -0400

    Gold bag spawn points on ship.

---
+++ Assets/Scripts/GoldBags.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldBags : MonoBehaviour
+{
+    public Transform[] goldBagSpawnPoints;
+    public GameObject goldBagPrefab;
+
+    public void AddGoldBagToShip()
+    {
+        int randomSpawnPoint = Random.Range(0, goldBagSpawnPoints.Length);
+        Instantiate(goldBagPrefab, goldBagSpawnPoints[randomSpawnPoint].transform.position, Quaternion.identity);
+    }
+}
